c60e9ad0c94152f97f5ee6eb00c06b77e2857716
Nikolai Mishchenko <progressivid@mail.ru>
2023-09-03 19:24:33 +0300

    6. Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -4,10 +4,12 @@ public class Broken {
 
     private int sizeOfEmpty = 10;
     public String surname;
-
+    String name;
     public final String newValue = "";
 
     void echo() { }
+
+    Broken() { }
 
     void media(Object obj) {
         if (obj != null) {
@@ -18,9 +20,4 @@ public class Broken {
     void method(int a, int b, int c, int d, int e, int f, int g) {
 
     }
-
-    String name;
-
-    Broken() { }
-
 }
