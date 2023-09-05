f733c8d8c3edc75ea93df77dcca901b642fd7b95
Nikolai Mishchenko <progressivid@mail.ru>
2023-09-05 09:30:48 +0300

    1.7. Конструктор [#173889 #442382]

--- src/main/java/ru/job4j/oop/Error.java
+++ src/main/java/ru/job4j/oop/Error.java
@@ -9,21 +9,18 @@ public class Error {
     private String message;
 
     public Error() {
-        active = true;
-        status = 2;
-        message = "Сообщение 1";
-    }
-
-    public void printInfo() {
-        System.out.println(active);
-        System.out.println(2);
-        System.out.println(message);
     }
 
     public Error(boolean active, int status, String message) {
         this.active = active;
         this.status = status;
         this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
     }
 
     public static void main(String[] args) {
