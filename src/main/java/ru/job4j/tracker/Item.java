63e92c41f2ebbb016e17166e8e2e7035485ae56b
Nikolai Mishchenko <progressivid@mail.ru>
2023-09-11 09:09:30 +0300

    10. toString [#310018 #444010]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -43,7 +43,10 @@ public class Item {
 
     @Override
     public String toString() {
-        return "Текущие дата и время после форматирования: "
-                + created.format(FORMATTER);
+        return "Item{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", created=" + created.format(FORMATTER)
+                + '}';
     }
 }
