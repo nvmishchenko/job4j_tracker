f96833373e41f2b4c033b284baac9e20cbb2c17c
Nikolai Mishchenko <progressivid@mail.ru>
2023-09-12 16:50:50 +0300

    9. Тестирование. Подготовка данных. [#182960]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -22,7 +22,7 @@ public class StartUI {
         }
     }
 
-    public static void deteleItem(Input input, Tracker tracker) {
+    public static void deleteItem(Input input, Tracker tracker) {
         System.out.println("=== Delete item ===");
         int id = input.askInt("Enter id: ");
         tracker.delete(id);
@@ -78,7 +78,7 @@ public class StartUI {
             } else if (select == 2) {
                 StartUI.editItem(input, tracker);
             } else if (select == 3) {
-                StartUI.deteleItem(input, tracker);
+                StartUI.deleteItem(input, tracker);
             } else if (select == 4) {
                 StartUI.findItemId(input, tracker);
             } else if (select == 5) {
