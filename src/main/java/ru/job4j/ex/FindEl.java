bf7b9bd85f0f98fecff827f2b6c5ae480cdf3674
Nikolai Mishchenko <progressivid@mail.ru>
2023-09-13 20:58:34 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (key.equals(value[i])) {
+                rsl = i;
+                break;
+            }
+            if (rsl == -1) {
+                throw new ElementNotFoundException("element not found");
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String[] value = new String[]{"one", "two", "three"};
+            indexOf(value, "second");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
