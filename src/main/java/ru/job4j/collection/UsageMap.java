06fcf3737e1351b5b73747eacfbae7e0b9f6addc
Nikolai Mishchenko <progressivid@mail.ru>
2023-09-27 15:03:08 +0300

    0. Map, HashMap [#212652 #447943]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,11 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("123@mail.ru", "Ivanov Ivan");
+        map.put("1234@mail.ru", "Ivanov Ivan");
+        map.put("12332@mail.ru", "Ivanov Ivan");
+        map.put("12398@mail.ru", "Ivanov Ivan98");
+        map.put("12378@mail.ru", "Ivanov Ivan78");
+        map.put("123@mail.ru", "Ivanov Ivan65");
         for (Map.Entry<String, String> entry : map.entrySet()) {
             String value = entry.getValue();
             System.out.println(value);
